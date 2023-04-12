61adf8865dd4af9627da3267594f62e6524672b5
Zachary Trundle <ztrunddev@gmail.com>
2023-04-12 10:46:59 -0500

    Turn on cdn for publishing

--- lib/sanityClient.ts
+++ lib/sanityClient.ts
@@ -3,8 +3,8 @@ import { createClient } from "@sanity/client";
 const sanityClient = createClient({
     projectId: "fcb9r3pv",
     dataset: "production",
-    apiVersion: "2023-03-19", // use current UTC date - see "specifying API version"!
-    useCdn: false, // `false` if you want to ensure fresh data
+    apiVersion: "2023-04-12", // use current UTC date - see "specifying API version"!
+    useCdn: true, // `false` if you want to ensure fresh data
 });
 
 export default sanityClient;
