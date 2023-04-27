d73fbccd5d732c9c78e340e804160ac472f40cd6
Zachary Trundle <ztrunddev@gmail.com>
2023-04-27 10:04:10 -0500

    Minor changes to presentation
    Added price to page with all the puppies. Reorganized individual puppy page to include financing banner, moved name, availability, and price to top of page, and changed birthdate to age. Reorganized individual parent page to have name on top of page, and changed birthdate to age.

--- pages/puppies/[name].tsx
+++ pages/puppies/[name].tsx
@@ -3,6 +3,8 @@ import sanityClient from '../../lib/sanityClient';
 import Layout from '../../components/layout';
 import CustomCarousel from "../../components/customCarousel";
 import {MediaItem} from "../../types";
+import Image from 'next/image';
+import {getAge} from "../../helpers/getAge";
 
 type PuppyProps = {
     name: string;
@@ -30,35 +32,46 @@ const Puppy = ({puppy}: InferGetStaticPropsType<typeof getStaticProps>) => {
         price,
     } = puppy;
 
+    const { years, weeks, days } = getAge(birthdate);
+
     return (
         <Layout pageTitle={name}>
+            <div className="flex justify-between p-2 mb-4 bg-light-shades shadow-lg rounded-lg">
+                <h1 className="text-3xl font-bold">{name}</h1>
+                <h1 className="text-3xl font-normal">{availability} - ${price}</h1>
+            </div>
             <div className="flex flex-col md:flex-row gap-4">
-                <div className="w-full md:w-1/2 h-min p-0 bg-light-shades drop-shadow-lg rounded-lg overflow-hidden">
+                <div className="w-full md:w-1/2 h-min p-0 bg-light-shades shadow-lg rounded-lg overflow-hidden">
                     <CustomCarousel mediaItems={mediaItems}/>
                 </div>
-                <div className="w-full md:w-1/2 h-min p-2 bg-light-shades drop-shadow-lg rounded-lg">
-                    <h1 className="text-3xl font-bold mb-2">{name}</h1>
-                    <p>
-                        <strong>Birthdate:</strong> {new Date(birthdate).toLocaleDateString()}
-                    </p>
-                    <p>
-                        <strong>Gender:</strong> {gender}
-                    </p>
-                    <p>
-                        <strong>Color:</strong> {color}
-                    </p>
-                    <p>
-                        <strong>Weight:</strong> {weight} lbs
-                    </p>
-                    <p>
-                        <strong>Description:</strong> {description}
-                    </p>
-                    <p>
-                        <strong>Availability:</strong> {availability}
-                    </p>
-                    <p>
-                        <strong>Price:</strong> ${price}
-                    </p>
+                <div className="w-full md:w-1/2">
+                    <div className="h-min p-2 bg-light-shades shadow-lg rounded-lg mb-4">
+                        <p>
+                            <strong>Age:</strong> {years > 0 ? `${years} ${years === 1 ? 'year' : 'years'},` : ''} {weeks} {weeks === 1 ? 'week' : 'weeks'} and {days} {days === 1 ? 'day' : 'days'} old
+                        </p>
+                        <p>
+                            <strong>Gender:</strong> {gender}
+                        </p>
+                        <p>
+                            <strong>Color:</strong> {color}
+                        </p>
+                        <p>
+                            <strong>Weight:</strong> {weight} lbs
+                        </p>
+                        <p>
+                            <strong>Description:</strong> {description}
+                        </p>
+                    </div>
+                    <div className="h-min p-0 bg-light-shades shadow-lg rounded-lg overflow-hidden">
+                        <a href="https://www.example.com/financing-application" target="_blank" rel="noopener noreferrer">
+                            <Image
+                                src="/TFC-rectangle-web-banner-800x500.png"
+                                alt="Terrace Finance - Financing Available"
+                                width='800'
+                                height='500'
+                            />
+                        </a>
+                    </div>
                 </div>
             </div>
         </Layout>
