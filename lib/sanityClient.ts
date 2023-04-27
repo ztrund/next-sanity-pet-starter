f917fd093dab7c0e5d414824c8a9ea87bd058275
Zachary Trundle <ztrunddev@gmail.com>
2023-04-27 08:17:19 -0500

    Fix hover and turn off cdn for dev

--- lib/sanityClient.ts
+++ lib/sanityClient.ts
@@ -4,7 +4,7 @@ const sanityClient = createClient({
     projectId: "fcb9r3pv",
     dataset: "production",
     apiVersion: "2023-04-12", // use current UTC date - see "specifying API version"!
-    useCdn: true, // `false` if you want to ensure fresh data
+    useCdn: false, // `false` if you want to ensure fresh data
 });
 
 export default sanityClient;
