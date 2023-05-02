75c98e93a46efd2312c8a6faf69b8474739350f0
Zachary Trundle <ztrunddev@gmail.com>
2023-05-02 15:21:07 -0500

    Switch to react-youtube package

--- components/youtubeLiveEmbed.tsx
+++ components/youtubeLiveEmbed.tsx
@@ -1,12 +1,18 @@
-import React, {useState, useEffect} from 'react';
-import {extractYoutubeChannelId, extractYoutubeVideoId} from '../helpers/youtubeLinkExtractor';
-import {YoutubeSettings} from "../types"; // Import the extractor functions
+import React, { useState, useEffect } from 'react';
+import YouTube from 'react-youtube';
+import {
+    extractYoutubeChannelId,
+    extractYoutubeVideoId,
+} from '../helpers/youtubeLinkExtractor';
+import { YoutubeSettings } from '../types';
 
 interface YoutubeLiveEmbedProps {
     youtubeSettings: YoutubeSettings;
 }
 
-const YoutubeLiveEmbed: React.FC<YoutubeLiveEmbedProps> = ({ youtubeSettings }) => {
+const YoutubeLiveEmbed: React.FC<YoutubeLiveEmbedProps> = ({
+                                                               youtubeSettings,
+                                                           }) => {
     const [videoId, setVideoId] = useState<string>('');
 
     useEffect(() => {
@@ -14,12 +20,16 @@ const YoutubeLiveEmbed: React.FC<YoutubeLiveEmbedProps> = ({ youtubeSettings }) 
             return;
         }
 
-        const channelId = extractYoutubeChannelId(youtubeSettings.channelUrl) || '';
-        const fallbackVideoId = extractYoutubeVideoId(youtubeSettings.fallbackVideoUrl) || '';
+        const channelId =
+            extractYoutubeChannelId(youtubeSettings.channelUrl) || '';
+        const fallbackVideoId =
+            extractYoutubeVideoId(youtubeSettings.fallbackVideoUrl) || '';
 
         const fetchLivestreamData = async () => {
             try {
-                const response: Response = await fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=${channelId}&eventType=live&type=video&key=${process.env.NEXT_PUBLIC_YOUTUBE_API_KEY}`);
+                const response: Response = await fetch(
+                    `https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=${channelId}&eventType=live&type=video&key=${process.env.NEXT_PUBLIC_YOUTUBE_API_KEY}`
+                );
                 const data: { items: { id: { videoId: string } }[] } = await response.json();
 
                 if (data.items.length > 0) {
@@ -35,15 +45,22 @@ const YoutubeLiveEmbed: React.FC<YoutubeLiveEmbedProps> = ({ youtubeSettings }) 
         fetchLivestreamData();
     }, [youtubeSettings]);
 
+    const opts = {
+        playerVars: {
+            autoplay: 1,
+            mute: 1,
+        },
+    };
+
     return (
         <div className="aspect-w-16 aspect-h-9">
-            <iframe
+            <YouTube
+                videoId={videoId}
+                opts={opts}
                 className="absolute inset-0 w-full h-full"
-                src={`https://www.youtube.com/embed/${videoId}?autoplay=1&mute=1`}
-                title="YouTube video player"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowFullScreen
-            ></iframe>
+                iframeClassName="absolute inset-0 w-full h-full"
+                onReady={(event) => event.target.playVideo()}
+            />
         </div>
     );
 };
