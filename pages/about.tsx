71c2c2d910d3a84cf8f048fdab305fe6706a1a4f
Zachary Trundle <ztrunddev@gmail.com>
2023-05-09 22:01:06 -0500

    Add team to about page

--- pages/about.tsx
+++ pages/about.tsx
@@ -3,16 +3,40 @@ import {PortableText} from '@portabletext/react';
 import Layout from "../components/layout";
 import CustomCarousel from "../components/customCarousel";
 import fetchPageData from "../lib/fetchPageData";
+import {TeamMember} from "../types";
+import imageUrlBuilder from "@sanity/image-url";
+import sanityClient from "../lib/sanityClient";
 
 const About = ({pageData}: InferGetStaticPropsType<typeof getStaticProps>) => {
     const {about, metaDescription} = pageData;
+    const imageBuilder = imageUrlBuilder(sanityClient);
     return (
         <Layout pageTitle="About Us"
                 metaDesc={metaDescription.description}
                 pageData={pageData}>
             <div className="flex flex-col md:flex-row gap-4">
-                <div className="w-full md:w-1/2 h-min p-2 bg-light-shades drop-shadow-lg rounded-lg">
-                    <div className="prose max-w-none"><PortableText value={about.content}/></div>
+                <div className="flex flex-col w-full md:w-1/2 h-min gap-4">
+                    <div className="p-2 bg-light-shades drop-shadow-lg rounded-lg">
+                        <h2 className="text-4xl font-extrabold text-center mb-2">About Us</h2>
+                        <div className="prose max-w-none"><PortableText value={about.content}/></div>
+                    </div>
+                    <div className="p-2 bg-light-shades drop-shadow-lg rounded-lg">
+                        <h2 className="text-4xl font-extrabold text-center mb-2">Our Team</h2>
+                        <div className="flex flex-wrap justify-center gap-2 mb-2">
+                            {about.team.map((teamMember: TeamMember) => (
+                                <div key={teamMember.name} className="flex flex-col md:flex-row items-center">
+                                    <div className="flex flex-col items-center">
+                                        <img
+                                            src={imageBuilder.image(teamMember.image).width(128).height(128).auto('format').quality(75).url()}
+                                            alt={teamMember.name} className="rounded-full shadow-lg"/>
+                                        <h3 className="text-xl font-bold mt-2 text-center">{teamMember.name}</h3>
+                                        <p className="text-sm text-center">{teamMember.position}</p>
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+                        <div className="prose max-w-none"><PortableText value={about.teamDescription}/></div>
+                    </div>
                 </div>
                 <div className="w-full md:w-1/2 h-min p-0 bg-light-shades drop-shadow-lg rounded-lg overflow-hidden">
                     <CustomCarousel mediaItems={about.mediaItems}/>
@@ -26,7 +50,13 @@ export const getStaticProps: GetStaticProps = async () => {
     const additionalQuery = `
     "about": *[_type == "about"][0] {
       content,
-      mediaItems
+      mediaItems,
+      team[] {
+        name,
+        position,
+        image,
+      },
+      teamDescription,
     },
     "metaDescription": *[_type == "metaDescriptions"][0]{
       'description': about,
