b97106de499ac193c39ed2425cf07a369f02ef5f
Zachary Trundle <ztrunddev@gmail.com>
2023-05-15 20:36:39 -0500

    Update headers

--- createHeaders.js
+++ createHeaders.js
@@ -5,15 +5,15 @@ const headers = `
 /*
   Strict-Transport-Security: max-age=63072000; includeSubDomains; preload
   X-Frame-Options: SAMEORIGIN
-  Content-Security-Policy: default-src 'self'; img-src 'self' https://i.ytimg.com https://*.sanity.io data:; media-src 'self'; script-src 'self'; style-src 'self' 'unsafe-inline'; frame-src 'self' https://www.youtube-nocookie.com/
+  Content-Security-Policy: default-src 'self'; img-src 'self' https://i.ytimg.com https://*.sanity.io data:; media-src 'self' ; script-src 'self' https://static.cloudflareinsights.com; style-src 'self' 'unsafe-inline'; frame-src 'self' https://www.youtube-nocookie.com/; connect-src 'self' https://*.sanity.io https://*.cloudflareinsights.com;
 `;
 
 const outputPath = path.join(__dirname, 'out');
-fs.mkdirSync(outputPath, { recursive: true });
+fs.mkdirSync(outputPath, {recursive: true});
 
 const headersPath = path.join(outputPath, '_headers');
-fs.writeFile(headersPath, headers, function(err) {
-    if(err) {
+fs.writeFile(headersPath, headers, function (err) {
+    if (err) {
         return console.log(err);
     }
     console.log("The file was saved!");
