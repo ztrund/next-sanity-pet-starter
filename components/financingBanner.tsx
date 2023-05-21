4a37cf0728e64c7425b99e0a0a4c4138e7d90d9c
Zachary Trundle <ztrunddev@gmail.com>
2023-05-21 01:51:55 -0500

    Refactor

--- components/financingBanner.tsx
+++ components/financingBanner.tsx
@@ -24,25 +24,25 @@ const FinancingBanner: React.FC<FinancingBannerProps> = ({financing}) => {
         return null;
     }
 
-    let imgDimensions = null;
-    if (financing.banner.asset._ref) {
-        imgDimensions = imageDimensionExtractor(financing.banner.asset._ref);
+    const imgDimensions = imageDimensionExtractor(financing.banner.asset._ref);
+
+    // If we couldn't extract dimensions, don't render the component
+    if (!imgDimensions) {
+        return null;
     }
 
     return (
-        imgDimensions ? (
-            <div className="h-min p-0 bg-light-shades shadow-lg rounded-lg overflow-hidden">
-                <a href={financing.link} target="_blank" rel="noopener noreferrer">
-                    <img
-                        src={imageBuilder.image(financing.banner).width(744).auto('format').quality(75).url()}
-                        alt="Financing Available"
-                        loading="lazy"
-                        width="744"
-                        height={imgDimensions.width / 744 * imgDimensions.height}
-                    />
-                </a>
-            </div>
-        ) : null
+        <div className="h-min p-0 bg-light-shades shadow-lg rounded-lg overflow-hidden">
+            <a href={financing.link} target="_blank" rel="noopener noreferrer">
+                <img
+                    src={imageBuilder.image(financing.banner).width(744).auto('format').quality(75).url()}
+                    alt="Financing Available"
+                    loading="lazy"
+                    width="744"
+                    height={imgDimensions.width / 744 * imgDimensions.height}
+                />
+            </a>
+        </div>
     );
 }
 
