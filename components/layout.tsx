50dfa8cfb2a0f923da4ed8b50c4ce54ed20aa612
Zachary Trundle <ztrunddev@gmail.com>
2023-05-22 10:31:58 -0500

    Add robots.txt and _headers

--- components/layout.tsx
+++ components/layout.tsx
@@ -27,11 +27,16 @@ const Layout = ({
             <Head>
                 <title>{pageTitle}</title>
                 <link rel="icon" href={imageBuilder.image(favicon).width(512).height(512).url()}/>
-                <link rel="icon" type="image/png" sizes="512x512" href={imageBuilder.image(favicon).width(512).height(512).format("png").url()} />
-                <link rel="icon" type="image/png" sizes="192x192" href={imageBuilder.image(favicon).width(192).height(192).format("png").url()} />
-                <link rel="icon" type="image/png" sizes="32x32" href={imageBuilder.image(favicon).width(32).height(32).format("png").url()} />
-                <link rel="icon" type="image/png" sizes="16x16" href={imageBuilder.image(favicon).width(16).height(16).format("png").url()} />
-                <link rel="apple-touch-icon" sizes="180x180" href={imageBuilder.image(favicon).width(180).height(180).format("png").url()} />
+                <link rel="icon" type="image/png" sizes="512x512"
+                      href={imageBuilder.image(favicon).width(512).height(512).format("png").url()}/>
+                <link rel="icon" type="image/png" sizes="192x192"
+                      href={imageBuilder.image(favicon).width(192).height(192).format("png").url()}/>
+                <link rel="icon" type="image/png" sizes="32x32"
+                      href={imageBuilder.image(favicon).width(32).height(32).format("png").url()}/>
+                <link rel="icon" type="image/png" sizes="16x16"
+                      href={imageBuilder.image(favicon).width(16).height(16).format("png").url()}/>
+                <link rel="apple-touch-icon" sizes="180x180"
+                      href={imageBuilder.image(favicon).width(180).height(180).format("png").url()}/>
                 <meta
                     name="description"
                     content={metaDesc}
