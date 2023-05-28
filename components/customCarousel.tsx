f1658938305450c42c9e5419651b54a0e10d1bf5
Zachary Trundle <ztrunddev@gmail.com>
2023-05-28 12:09:11 -0500

    Remove react-modal

--- components/customCarousel.tsx
+++ components/customCarousel.tsx
@@ -4,10 +4,8 @@ import {MediaItem} from "../types";
 import useEmblaCarousel from "embla-carousel-react";
 import Autoplay from "embla-carousel-autoplay";
 import LiteYouTubeEmbed from "react-lite-youtube-embed";
-import Modal from "react-modal";
 import {sanityImageUrl} from "../lib/sanityImageUrl";
-
-Modal.setAppElement('#root');
+import {createPortal} from "react-dom";
 
 interface CustomCarouselProps {
     mediaItems: MediaItem[];
@@ -17,15 +15,19 @@ const CustomCarousel: React.FC<CustomCarouselProps> = ({mediaItems}) => {
     const [selectedIndex, setSelectedIndex] = useState(0);
     const [showModal, setShowModal] = useState(false);
     const [currentImage, setCurrentImage] = useState("");
+    const [modalNode, setModalNode] = useState<Element | null>(null);
 
-    const handleImageClick = (imageUrl: string) => {
-        setCurrentImage(imageUrl);
-        setShowModal(true);
-    };
+    useEffect(() => {
+        setModalNode(document.getElementById('root'));
+    }, []);
 
-    const closeModal = () => {
-        setShowModal(false);
-    };
+    useEffect(() => {
+        if (showModal) {
+            document.body.classList.add("overflow-hidden");
+        } else {
+            document.body.classList.remove("overflow-hidden");
+        }
+    }, [showModal]);
 
     const [viewportRef, embla] = useEmblaCarousel(
         {loop: true},
@@ -61,23 +63,20 @@ const CustomCarousel: React.FC<CustomCarouselProps> = ({mediaItems}) => {
         }
     }, [embla]);
 
+    const modal = (
+        <div className={`${showModal ? "fixed" : "hidden"} inset-0 z-50 backdrop-blur-sm backdrop-brightness-50`}
+             onClick={() => setShowModal(false)}>
+            <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 outline-0"
+                 aria-modal={true}>
+                <img src={currentImage} className="rounded-lg overflow-hidden shadow-lg max-w-[95vw] max-h-[95vh]"
+                     onClick={() => setShowModal(false)} alt={"Modal Image"}/>
+            </div>
+        </div>
+    );
+
     return (
         <>
-            <Modal
-                isOpen={showModal}
-                onRequestClose={closeModal}
-                contentLabel="Image Modal"
-                bodyOpenClassName="overflow-hidden"
-                overlayClassName="fixed inset-0 z-50 backdrop-blur-sm backdrop-brightness-50"
-                className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 outline-0"
-            >
-                <img
-                    src={currentImage}
-                    alt="Popup"
-                    className="rounded-lg overflow-hidden shadow-lg max-w-[95vw] max-h-[95vh]"
-                    onClick={closeModal}
-                />
-            </Modal>
+            {modalNode && createPortal(modal, modalNode)}
             <div className="embla overflow-hidden" ref={viewportRef}>
                 <div className="embla__container flex items-center">
                     {mediaItems.map((mediaItem: MediaItem, index: number) => (
@@ -87,12 +86,21 @@ const CustomCarousel: React.FC<CustomCarouselProps> = ({mediaItems}) => {
                         >
                             {mediaItem.type === "image" && mediaItem.image && (
                                 <img
-                                    src={sanityImageUrl(mediaItem.image, {w: 744, h: 744, auto: "format", q: 75, fit: "crop"})}
+                                    src={sanityImageUrl(mediaItem.image, {
+                                        w: 744,
+                                        h: 744,
+                                        auto: "format",
+                                        q: 75,
+                                        fit: "crop"
+                                    })}
                                     alt={"Slide " + index}
                                     loading={index < 1 ? "eager" : "lazy"}
                                     width="744"
                                     height="744"
-                                    onClick={() => handleImageClick(sanityImageUrl(mediaItem.image, {auto: "format", q: 75}))}
+                                    onClick={() => {
+                                        setCurrentImage(sanityImageUrl(mediaItem.image, {auto: "format", q: 75}));
+                                        setShowModal(true);
+                                    }}
                                 />
                             )}
                             {mediaItem.type === "video" && mediaItem.videoUrl && (
@@ -123,7 +131,13 @@ const CustomCarousel: React.FC<CustomCarouselProps> = ({mediaItems}) => {
                                 {mediaItem.type === "image" && mediaItem.image && (
                                     <img
                                         key={index}
-                                        src={sanityImageUrl(mediaItem.image, {h: 128, w: 128, auto: "format", q: 75, fit: "crop"})}
+                                        src={sanityImageUrl(mediaItem.image, {
+                                            h: 128,
+                                            w: 128,
+                                            auto: "format",
+                                            q: 75,
+                                            fit: "crop"
+                                        })}
                                         height="128"
                                         width="128"
                                         alt={"Slide " + index + " Thumbnail"}
